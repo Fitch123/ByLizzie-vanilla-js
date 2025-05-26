86d92291362d75972bc3a80f50787579a1a1b0de
Cesar <cfitchf@wgu.edu>
2025-05-26 12:18:56 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+//HAMBURGER NAVBAR 
+const toggle = document.getElementById('menu-toggle');
+const navLink = document.querySelector('.nav-links');
+
+toggle.addEventListener('click', () => {
+    navLink.classList.toggle('show');
+});
+
+
+//Active navbar 
+
